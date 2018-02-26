04a369dd91db626e8feea07754e33eed54d0feca
Lawre <camperinc@gmail.com>
2018-02-26 19:08:44 +0300

    It works.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+loveIs = 0;
+    arrLover = [];
+    for(var i = 0; i<preferences.length; i ++) {
+        lover1 = (preferences[i] - 1);
+        lover2 = (preferences[lover1] - 1);
+        lover3 = (preferences[lover2] - 1);
+        if (lover1 != i && lover2 != i && lover3 == i && arrLover.indexOf(i) == -1) {
+            arrLover.push(i);
+            arrLover.push(lover1);
+            arrLover.push(lover2);
+            loveIs++;
+            }
+        }
+    return loveIs;
 };
